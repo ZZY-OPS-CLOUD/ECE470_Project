c95990122c0ada5c6b688cfe5fdbe5a99e6554c6
QuokeCola <QuokeCola@iCloud.com>
2021-10-16 17:47:41 -0500

    Update Documentation to Sphinx format. Update ur3.urdf.xacro, optimize the sensor location and collision model.

--- src/CTRL_py/scripts/LaserInterface.py
+++ src/CTRL_py/scripts/LaserInterface.py
@@ -5,11 +5,15 @@ from sensor_msgs.msg import LaserScan
 class LaserInterface:
 
     def __init__(self):
+        """initialize the laser interface. Add subscriber.
+        """
         self.scanned_data = []
         self.intensities = []
 
-        laserSub = rospy.Subscriber('/project_ctrl/laser/scan', LaserScan, self.__laser_callback)
+        self.laserSub = rospy.Subscriber('/project_ctrl/laser/scan', LaserScan, self.__laser_callback)
 
     def __laser_callback(self, msg):
+        """Callback function for process the feedback from laser scanner
+        """
         self.scanned_data = msg.ranges
         self.intensities = msg.intensities
--- src/CTRL_py/scripts/Ur3Interface.py
+++ src/CTRL_py/scripts/Ur3Interface.py
@@ -6,81 +6,80 @@ import rospkg
 
 
 class UR3Interface:
-    """
-    @brief init the interface, including register the callback functions and initialize variables.
-    """
 
-    def __init__(self, SPINRATE_=20):
-        self.angle_feedback = [0, 0, 0, 0, 0, 0]
-        self.gripper_feedback = False
-        self.target_reached = True
+    def __init__(self, spinrate_=20):
+        """Init the interface, including register the callback functions and initialize variables.
 
-        self.target_angle = [0, 0, 0, 0, 0, 0]
-        self.target_sprayer_state = False
-        self.target_speed = 0.0
-        self.target_acceleration = 0.0
+        :param spinrate_ the rate communicate with ROS
+        """
+        self._angle_feedback = [0, 0, 0, 0, 0, 0]
+        self._gripper_feedback = False
+        self._target_reached = True
 
-        self.cmd = rospy.Publisher('ur3/command', command, queue_size=10)
-        self.position = rospy.Subscriber('ur3/position', position, self.__pos_callback)
-        self.gripper = rospy.Subscriber('ur3/gripper_input', gripper_input, self.__gripper_callback)
-        self.SPINRATE = SPINRATE_
+        self._target_angle = [0, 0, 0, 0, 0, 0]
+        self._target_sprayer_state = False
+        self._target_speed = 0.0
+        self._target_acceleration = 0.0
 
-    """
-    @brief callback function for 6 joints' angle feedback
-    """
+        self._cmd = rospy.Publisher('ur3/command', command, queue_size=10)
+        self._position = rospy.Subscriber('ur3/position', position, self.__pos_callback)
+        self._gripper = rospy.Subscriber('ur3/gripper_input', gripper_input, self.__gripper_callback)
+        self._SPINRATE = spinrate_
 
     def __pos_callback(self, msg):
+        """Callback function for 6 joints' angle feedback
+        """
         for i in range(0, 6):
-            self.angle_feedback[i] = msg.position[i]
-
-    """
-    @brief callback function for laser's feedback
-    """
-
-    # TODO: modified the callback function for laser
+            self._angle_feedback[i] = msg.position[i]
 
     def __gripper_callback(self, msg):
-        self.gripper_feedback = msg.DIGIN
-
-    """
-    @brief Set gripper of UR3
-    @param gripper_state_ (True/False) whether the gripper is on or off
-    """
+        """Callback function for gripper's feedback
+        """
+        self._gripper_feedback = msg.DIGIN
 
     def set_sprayer(self, sprayer_state_):
-        self.target_sprayer_state = sprayer_state_
+        """Set gripper of UR3
+
+        :param sprayer_state_ (True/False) whether the gripper is on or off
+        """
+        self._target_sprayer_state = sprayer_state_
 
     def set_angle(self, target_angle_, speed_, acceleration_):
-        # for i in range(0, 6):
-        #     if abs(self.target_angle[i] - self.angle_feedback[i]) > 0.0005:
-        #         return False
+        """Set gripper of UR3
+
+        :param target_angle_ target angle of 6 joints
+        :param speed_ moving speed of UR3
+        :param acceleration_ acceleration of UR3
+        """
         for i in range(0, 6):
-            self.target_angle[i] = target_angle_[i]
+            self._target_angle[i] = target_angle_[i]
         if speed_ <= 0.0:
             return
 
-        self.target_speed = speed_
-        self.target_acceleration = acceleration_
+        self._target_speed = speed_
+        self._target_acceleration = acceleration_
         self.__publish_variables()
-        self.target_reached = False
+        self._target_reached = False
         spin_count = 0
-        while not self.target_reached:
-            self.target_reached = (abs(self.target_angle[0] - self.angle_feedback[0]) < 0.0005) and \
-                                  (abs(self.target_angle[1] - self.angle_feedback[1]) < 0.0005) and \
-                                  (abs(self.target_angle[2] - self.angle_feedback[2]) < 0.0005) and \
-                                  (abs(self.target_angle[3] - self.angle_feedback[3]) < 0.0005) and \
-                                  (abs(self.target_angle[4] - self.angle_feedback[4]) < 0.0005) and \
-                                  (abs(self.target_angle[5] - self.angle_feedback[5]) < 0.0005)
+        while not self._target_reached:
+            self._target_reached = (abs(self._target_angle[0] - self._angle_feedback[0]) < 0.0005) and \
+                                   (abs(self._target_angle[1] - self._angle_feedback[1]) < 0.0005) and \
+                                   (abs(self._target_angle[2] - self._angle_feedback[2]) < 0.0005) and \
+                                   (abs(self._target_angle[3] - self._angle_feedback[3]) < 0.0005) and \
+                                   (abs(self._target_angle[4] - self._angle_feedback[4]) < 0.0005) and \
+                                   (abs(self._target_angle[5] - self._angle_feedback[5]) < 0.0005)
 
-            if spin_count > self.SPINRATE * 5:
+            if spin_count > self._SPINRATE * 5:
                 self.__publish_variables()
                 spin_count = 0
             spin_count = spin_count + 1
 
     def __publish_variables(self):
+        """Publish the variables stored in the class to ros.
+        """
         ur3_msg = command()
-        ur3_msg.destination = self.target_angle
-        ur3_msg.v = self.target_speed
-        ur3_msg.a = self.target_acceleration
-        ur3_msg.io_0 = self.target_sprayer_state
-        self.cmd.publish(ur3_msg)
+        ur3_msg.destination = self._target_angle
+        ur3_msg.v = self._target_speed
+        ur3_msg.a = self._target_acceleration
+        ur3_msg.io_0 = self._target_sprayer_state
+        self._cmd.publish(ur3_msg)

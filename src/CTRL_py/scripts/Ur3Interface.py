2482146912b22881a3ff9d847a00bddcfa5bb224
ZZY-OPS-CLOUD <58516286+ZZY-OPS-CLOUD@users.noreply.github.com>
2021-11-13 13:46:43 -0600

    Update Ur3Interface.py

--- src/CTRL_py/scripts/Ur3Interface.py
+++ src/CTRL_py/scripts/Ur3Interface.py
@@ -83,3 +83,67 @@ class UR3Interface:
         ur3_msg.a = self._target_acceleration
         ur3_msg.io_0 = self._target_sprayer_state
         self._cmd.publish(ur3_msg)
+    def Get_MS():
+	    M = np.array([[0,-1,0,(540.0-150.0)/1000],[0,0,-1,(150.0+120.0-93.0+83.0+82.0+59.0)/1000],[1,0,0,(10+152+53.5)/1000],[0,0,0,1]])
+	    S1 = [0,0,1,0.15,0.15,0]
+	    S2 = [0,1,0,-0.162,0,-0.15]
+	    S3 = [0,1,0,-0.162,0,0.094]
+	    S4 = [0,1,0,-0.162,0,0.3070]
+	    S5 = [1,0,0,0,0.1620,-0.2600]
+	    S6 = [0,1,0,-0.1620,0,0.3900]
+	    S = [S1,S2,S3,S4,S5,S6]
+	    return M, S
+    def skew(vector):
+        return np.array([[0, -vector[2], vector[1], vector[3]],
+                        [vector[2], 0, -vector[0], vector[4]],
+                        [-vector[1], vector[0], 0, vector [5]],
+                        [0, 0, 0, 0]])
+    def lab_fk(theta1, theta2, theta3, theta4, theta5, theta6):
+	    M,S = Get_MS()
+	    theta = [theta1, theta2, theta3, theta4, theta5, theta6]
+	    return_value = [None, None, None, None, None, None]
+	    T = np.identity(4)
+	    for i in range(6):
+    		    T = np.matmul(T,expm(skew(S[i])*theta[i]))
+	    T = np.matmul(T,M)
+	    print("Foward kinematics calculated:\n")
+	    print(str(T) + "\n")
+	    return_value[0] = theta1 + PI
+	    return_value[1] = theta2
+	    return_value[2] = theta3
+	    return_value[3] = theta4 - (0.5*PI)
+	    return_value[4] = theta5
+	    return_value[5] = theta6
+	    return return_value
+    def lab_invk(xWgrip, yWgrip, zWgrip, yaw_WgripDegree):
+	    L1 = 0.152
+	    L2 = 0.12
+	    L3 = 0.244
+	    L4 = 0.093
+	    L5 = 0.213
+	    L6 = 0.083
+	    L7 = 0.083
+	    L8 = 0.082
+	    L9 = 0.0535
+	    L10 = 0.059 
+	    xgrip = xWgrip + 0.15
+	    ygrip = yWgrip - 0.15
+	    zgrip = zWgrip - 0.01
+	    xcen = xgrip - L9 * np.cos(np.deg2rad(yaw_WgripDegree))
+	    ycen = ygrip - L9 * np.sin(np.deg2rad(yaw_WgripDegree))
+	    zcen = zgrip
+	    alpha = np.arctan2(0.027 + L6, np.sqrt(xcen**2+ycen**2-(L6 + 0.027)**2))
+	    theta1 = np.arctan2(ycen,xcen) -  alpha
+        theta6 = np.pi/2 + theta1-np.deg2rad(yaw_WgripDegree)
+        x3end = xcen - L7*np.cos(theta1) + (0.027+L6)*np.sin(theta1)
+	    y3end = ycen - L7*np.sin(theta1) - (0.027+L6)*np.cos(theta1)
+	    z3end = zcen + L10 + L8
+	    M1 = np.sqrt(x3end**2 + (z3end-L1)**2 + y3end**2)
+	    omega = np.arctan2((z3end - L1), np.sqrt(x3end**2+y3end**2))
+	    theta3 = np.pi - np.arccos((L3**2 + L5**2 - M1**2)/(2*L3*L5))
+	    theta2 = -(omega + np.arccos((L3**2 + M1**2 - L5**2)/(2*L3*M1)))
+	    theta4 = -(theta3 + theta2)
+	    theta5 = -np.pi/2
+	    output_theta = lab_fk(float(theta1), float(theta2), float(theta3), float(theta4), float(theta5), float(theta6))
+	    print (theta1, theta2, theta3, theta4, theta5, theta6)
+	    return output_theta
